7d4009a8b85c3b5c21fde5adbc8094ad81472d1e
Priyanka <priyankad230596@gmail.com>
2026-06-09 17:48:47 +0530

    branch practice2

---
+++ tests/specs/regression.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test'
+// import { basepage } from '../../pages/basepage'
+import { ProductPage } from '../../pages/ProductPage'
+
+test.describe('validate product page copy',()=>{
+    let productPage:ProductPage
+    test.beforeEach(({page})=>{
+        productPage=new ProductPage(page)
+
+
+    })
+
+//click on about page 
+test('verify aboutpage link copy', async ({ page }) => {
+    await productPage.goTo_ProductPage()
+    await productPage.open_aboutPage()
+    expect(page.locator('//button[text()="Try it free"]')).toBeVisible()
+})
+})
+
